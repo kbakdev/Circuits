97994af6cb086cc23c97695db57500689ed0ce48
53jk1 <bonczalke@gmail.com>
2021-01-27 22:03:01 +0100

    variables added

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -24,16 +24,63 @@ namespace WindowsFormsApp1
 
 		private void button1_Click_1(object sender, EventArgs e)
 		{
+			int R1 = Int32.Parse(textBox1.Text);
+			int R2 = Int32.Parse(textBox2.Text);
+			int R3 = Int32.Parse(textBox3.Text);
+			int R4 = Int32.Parse(textBox4.Text);
+			int R5 = Int32.Parse(textBox5.Text);
+			int R6 = Int32.Parse(textBox6.Text);
+			int R7 = Int32.Parse(textBox7.Text);
+			int R8 = Int32.Parse(textBox8.Text);
+			int R9 = Int32.Parse(textBox9.Text);
+			int R10 = Int32.Parse(textBox10.Text);
+
+			int J1 = Int32.Parse(textBox11.Text);
+			int J2 = Int32.Parse(textBox12.Text);
+
+			int E1 = Int32.Parse(textBox13.Text);
+			int E2 = Int32.Parse(textBox14.Text);
+			int E3 = Int32.Parse(textBox15.Text);
+
 
 			DataTable dt = new DataTable();
-			dt.Columns.Add("1");
-			dt.Columns.Add("2");
-			dt.Columns.Add("3");
-			dt.Columns.Add("4");
+			dt.Columns.Add("Kolumna 1");
+			dt.Columns.Add("Kolumna 2");
+			dt.Columns.Add("Kolumna 3");
+			dt.Columns.Add("Kolumna 4");
 
-			dt.Rows.Add(new object[] { "1", "2", "3", "4" });
+			dt.Rows.Add(new object[] { R1, R2, R3, R4 });
 
 			dataGridView1.DataSource = dt;
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void label11_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
 		}
 	}
 }
